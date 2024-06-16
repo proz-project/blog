1ae9510a74a3f27e3ba6a6661b804a1f4249b21a
Renato <renatosilsoa@gmail.com>
2024-06-16 19:25:25 -0300

    troca de tela login e cadastro com js

--- src/login.js
+++ src/login.js
@@ -1,18 +1,18 @@
-const formContainer = document.querySelector(".form_container")
-const BtnCadastro = document.querySelector("#signup")
-const loginBtn = document.querySelector("#login")
-const OcultarAmostra = document.querySelectorAll(".pw_hide")
+const formOpenBtn = document.querySelector("#form-open");
+const home = document.querySelector(".home");
+const formContainer = document.querySelector(".form_container");
+const formCloseBtn = document.querySelector(".form_close");
+const signupBtn = document.querySelector("#signup");
+const loginBtn = document.querySelector("#login");
+const pwSchowhide = document.querySelector(".pw_hide");
 
 
-BtnCadastro.addEventListener("click", (e)=>{
-e.preventDefault();
-formContainer.classList.add("active")
-console.log(1)
-})
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
 
-
-loginBtn.addEventListener("click", (e)=>{
-e.preventDefault();
-console.log(1)
-formContainer.classList.remove("active")
-})
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
