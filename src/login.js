fc02d5ef56ead4c6ab4f0782038dc1ad9f7a1425
Renato <renatosilsoa@gmail.com>
2024-06-16 22:56:26 -0300

    valiação da tela de login

--- src/login.js
+++ src/login.js
@@ -1,21 +1,71 @@
-const formOpenBtn = document.querySelector("#form-open");
-const home = document.querySelector(".home");
-const formContainer = document.querySelector(".form_container");
-const formCloseBtn = document.querySelector(".form_close");
-const signupBtn = document.querySelector("#signup");
-const loginBtn = document.querySelector("#login");
+// variaveis
 const pwSchowhide = document.querySelectorAll(".pw_hide");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let form = document.querySelector("form");
+let textForm = document.getElementById("textForm");
+let textEmail = document.getElementById("textEmail");
+let textpassword = document.getElementById("textpassword");
 
+// ocultar e mostrar a senha
 pwSchowhide.forEach((icon) => {
   icon.addEventListener("click", () => {
     let gettPwInput = icon.parentElement.querySelector("input");
     if (gettPwInput.type === "password") {
       gettPwInput.type = "text";
-      icon.classList.replace("uil-eye-slash", "uil-eye")
-    }else{
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
       gettPwInput.type = "password";
-      icon.classList.replace("uil-eye-", "uil-eye-slash")
-
+      icon.classList.replace("uil-eye-", "uil-eye-slash");
     }
   });
 });
+
+// para verificar se está vazia os campos e passar a mensagem
+form.addEventListener("submit", (e) =>{
+  if(email.value== " " && password.value == ""){
+    textForm.textContent = "Você precisa preenher todos os campos"
+  }else if(validadorEmail(email.value) === true && validadorPassword(password.value) === true){
+    console.log(email.value);
+    console.log(password.value);
+    textForm.textContent = ' ';
+    textEmail.textContent = ' ';
+    textpassword.textContent = ' ';
+  }else{
+    console.log("requisição não atendida")
+
+  }
+  e.preventDefault()
+});
+
+// vERIFICAR DIGITOS
+email.addEventListener("keyup", () => {
+  if (validadorEmail(email.value) !== true) {
+    textEmail.textContent = "exemplo formato com letra minuscula: nome@gmail.com";
+  } else {
+    textEmail.textContent = " ";
+  }
+});
+
+password.addEventListener("keyup", () => {
+  if (validadorPassword(password.value) !== true) {
+    textpassword.textContent =
+      "formato da senha deve ter no mínimo 6 caracteres, contendo pelo menos um especial e uma letra em maiúscula";
+  } else {
+    textpassword.textContent = " ";
+  }
+});
+
+//função validação do email
+function validadorEmail(email) {
+  let emailPattern =
+    /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+  return emailPattern.test(email);
+}
+
+//função de validação da senha
+function validadorPassword(password) {
+  let passwordPattern =
+    /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+  return passwordPattern.test(password);
+}
