348dc2b02b30f350248f67e30abf832783389af5
Renato <renatosilsoa@gmail.com>
2024-06-17 01:38:24 -0300

    validação da tela de cadastro

--- src/login.js
+++ src/login.js
@@ -2,11 +2,14 @@
 const pwSchowhide = document.querySelectorAll(".pw_hide");
 let email = document.getElementById("email");
 let password = document.getElementById("password");
-let form = document.querySelector("form");
+let passwordTeste = document.getElementById("password-test");
+const form = document.querySelector("form");
 let textForm = document.getElementById("textForm");
 let textEmail = document.getElementById("textEmail");
-let textpassword = document.getElementById("textpassword");
-
+let textpassword = document.getElementById("textPassword");
+let textpasswordTeste = document.getElementById("textpassword-test");
+const campos = document.querySelectorAll(".required");
+const small = document.querySelectorAll(".small-required");
 // ocultar e mostrar a senha
 pwSchowhide.forEach((icon) => {
   icon.addEventListener("click", () => {
@@ -16,43 +19,52 @@ pwSchowhide.forEach((icon) => {
       icon.classList.replace("uil-eye-slash", "uil-eye");
     } else {
       gettPwInput.type = "password";
-      icon.classList.replace("uil-eye-", "uil-eye-slash");
+      icon.classList.replace("uil-eye", "uil-eye-slash");
     }
   });
 });
 
 // para verificar se está vazia os campos e passar a mensagem
-form.addEventListener("submit", (e) =>{
-  if(email.value== " " && password.value == ""){
-    textForm.textContent = "Você precisa preenher todos os campos"
-  }else if(validadorEmail(email.value) === true && validadorPassword(password.value) === true){
+form.addEventListener("submit", (e) => {
+  if (email.value == " " && password.value == "") {
+    textForm.textContent = "Você precisa preencher todos os campos";
+  } else if (
+    validadorEmail(email.value) === true &&
+    validadorPassword(password.value) === true
+  ) {
     console.log(email.value);
     console.log(password.value);
-    textForm.textContent = ' ';
-    textEmail.textContent = ' ';
-    textpassword.textContent = ' ';
-  }else{
-    console.log("requisição não atendida")
-
+    textForm.textContent = " ";
+    textEmail.textContent = " ";
+    textpassword.textContent = " ";
+    textpasswordTeste.textContent = " ";
+  } else {
+    console.log("Requisição não atendida");
   }
-  e.preventDefault()
+  e.preventDefault();
 });
 
-// vERIFICAR DIGITOS
+// vERIFICAR DIGITOS no email
 email.addEventListener("keyup", () => {
   if (validadorEmail(email.value) !== true) {
-    textEmail.textContent = "exemplo formato com letra minuscula: nome@gmail.com";
+    textEmail.textContent =
+      "Exemplo formato com letra minúscula: nome@gmail.com";
+    setError(0);
   } else {
     textEmail.textContent = " ";
+    removeError(0);
   }
 });
 
+// VERIFIANDO A SENHA principal DIGITO POR DIGITO
 password.addEventListener("keyup", () => {
   if (validadorPassword(password.value) !== true) {
     textpassword.textContent =
-      "formato da senha deve ter no mínimo 6 caracteres, contendo pelo menos um especial e uma letra em maiúscula";
+      "Formato da senha deve ter no mínimo 6 caracteres, contendo pelo menos um caracter especial.";
+    setError(1);
   } else {
     textpassword.textContent = " ";
+    removeError(1);
   }
 });
 
@@ -68,4 +80,36 @@ function validadorPassword(password) {
   let passwordPattern =
     /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
   return passwordPattern.test(password);
+}
+
+//função de comparação
+function CompararValores() {
+  if (passwordTeste.value.length < 6) {
+    setError(2);
+    textpasswordTeste.textContent =
+    "Senha incorreta.";
+  } else {
+    removeError(2);
+    comparar();
+  }
+}
+
+// comparar
+function comparar() {
+  if (campos[1].value === campos[2].value && campos[2].value.length >= 6) {
+    removeError(2);
+  } else {
+    setError(2);
+    textpasswordTeste.textContent =
+    "Senha incorreta.";
+  }
+}
+
+//função para aplicar cor na borda
+function setError(index) {
+  campos[index].style.border = " 1px solid #e63636";
+}
+
+function removeError(index) {
+  campos[index].style.border = "";
 }
